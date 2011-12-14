a962aed8d5360dfe92e5deaff93b94383066ad77
Ivan Perez <ivan.perez@keera.es>
2011-12-14 17:54:58 +0100

    Simple code that does the job

---
+++ src/Network/SMS/DiamondCard.hs
@@ -0,0 +1,66 @@
+-- | This module provides two main elements:
+-- * An operation to contact DiamondCard and send SMSs using their website
+-- * A datatype to process the result.
+--
+-- With the current implementation, the result page is not parsed.  Since the
+-- SOAP interface is not used either, we don't know if the message will be
+-- delivered correctly. We only know if the page loaded.
+--
+-- This issue is reflected in the datatype, which offers two constructors:
+-- SMSNotSent (which reflects the fact that we know for sure that the message
+-- will not be delivered) and SMSApparentlySent (which reflects that it might,
+-- it might not be sent).
+module Network.SMS.DiamondCard
+  ( SMSDeliveryResult
+  , sendSMS
+  )
+ where
+
+-- External imports
+import Data.IORef
+import Network.Curl
+
+-- | The result of sending an SMS. Since errors could be reported as part of the page
+-- text, it's difficult to tell if something went wrong. The possible output
+-- states are that a message might have been sent, or that it definitely wasn't.
+data SMSDeliveryResult = SMSApparentlySent
+                       | SMSNotSent String
+ deriving (Eq)
+
+-- | Send an sms using diamondcard's web interface.
+-- The parameters are:
+-- accountID
+-- pinCode
+-- message text (don't escape double quotes)
+-- from number (with international code and without plus sign)
+-- to number list (with international codes and no plus signs)
+sendSMS :: String -> String -> String -> String -> [ String ] -> IO SMSDeliveryResult
+sendSMS accID pinCode msg from tos = do
+
+  -- Initialise a curl op
+  h   <- initialize
+
+  -- Post output as a list of strings
+  ref <- newIORef []
+
+  -- Add post params, url and output collector
+  setopts h [ CurlVerbose False, CurlPostFields message
+            , CurlURL url, CurlWriteFunction (gatherOutput ref)
+            ]
+
+  -- Gather the results
+  rc  <- perform h
+  lss <- readIORef ref
+
+  return $ case rc of
+             CurlOK -> SMSApparentlySent
+             _      -> SMSNotSent $ concat $ reverse lss
+        
+  where url = "https://www.diamondcard.us/exec/voip-login"
+        message = [ "accId=" ++ accID
+                  , "pinCode=" ++ pinCode
+                  , "act=sms"
+                  , "msg=" ++ msg
+                  , "from=" ++ from
+                  ] ++ to
+        to = map ("to=" ++) tos
